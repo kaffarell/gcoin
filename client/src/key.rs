9cd61798b91e81312ed9af64ca3eb58d2d4a1648
kaffarell <gabrielgoller123@gmail.com>
2021-02-26 22:36:28 +0100

    Added signature

--- client/src/key.rs
+++ client/src/key.rs
@@ -1,12 +1,54 @@
 use openssl::rsa::{Rsa, Padding};
+use openssl::pkey::PKey;
+use openssl::sign::{Signer, Verifier};
+use openssl::hash::MessageDigest;
 use openssl::symm::Cipher;
+use pem::{Pem, encode, parse};
+use std::fs;
+use crate::data;
 
-pub fn generate_keys(passphrase: String){
+pub fn generate_keys(){
     let rsa = Rsa::generate(1024).unwrap();
-    let private_key: Vec<u8> = rsa.private_key_to_pem_passphrase(Cipher::aes_128_cbc(), 
-                                    passphrase.as_bytes()).unwrap();
+    let private_key: Vec<u8> = rsa.private_key_to_pem().unwrap(); 
     let public_key: Vec<u8> = rsa.public_key_to_pem().unwrap();
 
-    println!("Private key: {}", String::from_utf8(private_key).unwrap());
-    println!("Public key: {}", String::from_utf8(public_key).unwrap());
+    let private_key_string = String::from_utf8(private_key).unwrap();
+    let public_key_string = String::from_utf8(public_key).unwrap();
+
+    println!("{}", private_key_string);
+    println!("{}", public_key_string);
+    /*
+    let public_keys = rsa.public_key_to_der().unwrap();
+    let private_keys = rsa.private_key_to_der().unwrap();
+
+    let private_pem = Pem {
+        tag: String::from("RSA PRIVATE KEY"),
+        contents: private_keys,
+    };
+    let private = encode(&private_pem);
+    println!("------------------------------------");
+    println!("{}", private);
+    */
+    // Write to file
+    fs::write("./priv.pem", private_key_string).expect("Unable to write public key file");
+    fs::write("./pub.pem", public_key_string).expect("Unable to write private key file");
+}
+
+pub fn read_key(transaction: data::Transaction) {
+    // To String
+    let pub_data =  fs::read_to_string("./pub.pem").expect("Unable to read public key file");
+    let priv_data =  fs::read_to_string("./priv.pem").expect("Unable to read private key file");
+
+    // To rsa
+    let pub_rsa = Rsa::public_key_from_pem(&pub_data.as_bytes());
+    let priv_rsa = Rsa::private_key_from_pem(&priv_data.as_bytes()).expect("Error pem to private key");
+    let keypair = PKey::from_rsa(priv_rsa).unwrap();
+
+    // Sign the data
+    let data = format!("{}", transaction);
+    let mut signer = Signer::new(MessageDigest::sha256(), &keypair).unwrap();
+    signer.update(data.as_bytes()).unwrap();
+    let signature = signer.sign_to_vec().unwrap();
+    // Signature
+    println!("{}", String::from_utf8_lossy(&signature));
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -7,13 +7,15 @@ fn main() {
     // Check if new public/private key or use existing one
     println!("Create new private/public key? \n[1] yes \n[2] no");
     let mut line = String::new();
-    let b = stdin().read_line(&mut line).unwrap();
+    let _ = stdin().read_line(&mut line).unwrap();
     let choice_number: i32 = line.trim_end().parse().expect("Error parsing to i32");
     if choice_number == 1 {
-        // Generate public private key
-        key::generate_keys("test".to_string());
+        // Get passphrase
+        key::generate_keys();
     }else if choice_number == 2 {
-        // Read public private key from file
+        // Sign
+        let tran: data::Transaction = data::Transaction{sender: "204985".to_string(), receiver: "34573495".to_string(), amount: "3.4".to_string()};
+        key::read_key(tran)
     }else{
         println!("Input error");
     }
