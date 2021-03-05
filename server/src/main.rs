c9d50bb0abe643e68917ee4f9108e690247a42da
kaffarell <gabrielgoller123@gmail.com>
2021-03-05 21:30:27 +0100

    Added signature validation

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,17 +8,22 @@ use blockchain::block::*;
 use blockchain::chain::*;
 use payload::data::Transaction;
 use chrono::prelude::*;
+use rocket::http::Status;
 
 #[macro_use] extern crate rocket;
 
 
 #[post("/add", data = "<transaction>")] 
-async fn add_transaction(transaction: String) -> String{
+async fn add_transaction(transaction: String) -> Status{
     let chain: Chain = Chain::new();
     let mut block: Block = Block::new();
     let mut data: Vec<Transaction> = Vec::new();
     let t: Transaction = serde_json::from_str(&transaction).unwrap();
     // Check if transaction is signed correctly
+    if utils::crypto::verify_transaction(&t) == false {
+        return Status::BadRequest;
+    }
+    
     data.push(t);
     // Get time
     let time: DateTime<Local> = Local::now();
@@ -29,7 +34,7 @@ async fn add_transaction(transaction: String) -> String{
         add_block_to_chain(block, chain).await;
     });
 
-    return "OK".to_string();
+    return Status::Accepted;
 }
 
 #[get("/chain")]
@@ -39,9 +44,10 @@ fn get_chain() -> String {
 }
 
 async fn add_block_to_chain(mut block: Block, mut chain: Chain) {
-    println!("Added blocks");
+    println!("Adding blocks...");
     block.mine();
     chain.add(block);
+    println!("Added blocks");
 }
 
 
--- server/src/utils/crypto.rs
+++ server/src/utils/crypto.rs
@@ -1,6 +1,29 @@
 use md5;
+use openssl::sign::{Verifier};
+use openssl::rsa::Rsa;
+use openssl::pkey::PKey;
+use openssl::hash::MessageDigest;
+use crate::payload::data::Transaction;
 
 pub fn hash_md5<T: AsRef<[u8]>>(t: T) -> String {
     let digest = md5::compute(t);
     return format!("{:x}", digest);
+}
+
+pub fn verify_transaction(transaction: &Transaction) -> bool {
+    println!("Verifying transaction");
+    let mut pub_key_string: String = "-----BEGIN PUBLIC KEY-----\n".to_string();
+    pub_key_string.push_str(&transaction.sender[..]);
+    pub_key_string.push_str("\n-----END PUBLIC KEY-----");
+
+    let pub_key = Rsa::public_key_from_pem(&pub_key_string.as_bytes()).expect("Error pem to public key");
+    let keypair = PKey::from_rsa(pub_key).unwrap();
+
+    let data = format!("{}", transaction);
+    let signature = &transaction.signature;
+
+
+    let mut verifier = Verifier::new(MessageDigest::sha256(), &keypair).unwrap();
+    verifier.update(data.as_bytes()).unwrap();
+    return verifier.verify(&signature[..]).unwrap();
 }
